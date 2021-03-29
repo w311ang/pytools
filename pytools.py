b20c1dbe93480670dae79629db410609c687b07e
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-29 20:52:52 +0800

    Update pytools.py

--- pytools.py
+++ pytools.py
@@ -4,9 +4,9 @@ qfrom=''
 def update(**kw):
   global qpass,qfrom
   if 'pass' in kw:
-    qpass=kw['pass']
+    qpass=kw['qpass']
   if 'from' in kw:
-    qfrom=kw['from']
+    qfrom=kw['qfrom']
 
 def qmail(fromName,content,subject):
   import smtplib
@@ -28,17 +28,3 @@ def qmail(fromName,content,subject):
       server.quit()  # 关闭连接
   mail()
   print('邮件已发送')
-
-def ping(host):
-    """
-    Returns True if host (str) responds to a ping request.
-    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
-    """
-
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-
-    # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host]
-
-    return subprocess.call(command) == 0
