eb669d12a587e75f60a9ba43bfbd7f4712985c3d
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-29 21:06:51 +0800

    Update pytools_test.py

--- pytools_test.py
+++ pytools_test.py
@@ -5,4 +5,4 @@ qpass=os.getenv('qpass')
 qfrom=os.getenv('qfrom')
 pytools.update(qpass=qpass,qfrom=qfrom)
 test1=pytools.qmail('pytools_test','pytools_test','pytools_test')
-assert test1=None
+assert test1==None
