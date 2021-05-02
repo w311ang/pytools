039602fe784989a14435e98d7c431361a57662b1
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-02 22:44:14 +0800

    Update pytools_test.py

--- pytools_test.py
+++ pytools_test.py
@@ -7,5 +7,6 @@ pytools.update(qpass=qpass,qfrom=qfrom)
 def test_passing():
   qmail_test=pytools.qmail('pytools_test','pytools_test','pytools_test')
   assert qmail_test==None
+def test_passing():
   jmail_test=pytools.jmail('pytools_test','pytools_test','pytools_test')
   assert jmail_test==None
