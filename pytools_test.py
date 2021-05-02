49ff011bf34fe67565154c5434cdb830f43b831d
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-02 22:49:34 +0800

    Update pytools_test.py

--- pytools_test.py
+++ pytools_test.py
@@ -1,12 +1,14 @@
 import pytools
 import os
 
-qpass=os.getenv('qpass')
-qfrom=os.getenv('qfrom')
-pytools.update(qpass=qpass,qfrom=qfrom)
+@pytest.mark.skip
 def test_passing():
+  qpass=os.getenv('qpass')
+  qfrom=os.getenv('qfrom')
+  pytools.update(qpass=qpass,qfrom=qfrom)
   qmail_test=pytools.qmail('pytools_test','pytools_test','pytools_test')
   assert qmail_test==None
+
 def test_passing():
   jmail_test=pytools.jmail('pytools_test','pytools_test','pytools_test')
   assert jmail_test==None
