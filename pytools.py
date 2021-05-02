7c0c4bbd91fcaa1bb86a9bb24cf897bb75d68fc8
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-02 23:39:16 +0800

    Update pytools.py

--- pytools.py
+++ pytools.py
@@ -44,7 +44,7 @@ def jmail(fromName,subject,content,html=False):
   key=os.getenv('jmail')
   js=requests.get('https://raw.githubusercontent.com/w311ang/pytools/main/jmail.txt').text
   js=aes.AESCipher(key).decrypt(js)
-  print(qpass,qfrom)
+  print(type(qpass),type(qfrom))
   if not qpass and qfrom:
     js=json.loads(js)
     rqpass=js['qpass']
