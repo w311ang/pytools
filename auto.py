71ab35a30fe403cdd52bf2cde3ed881c7b99c9e4
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-02 22:09:55 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,8 +1,9 @@
 import aes
+import os
 
 key=os.getenv('key')
 js=os.getenv('json')
 
 output=aes.AESCipher(key).encrypt(js)
-with open('qmail.txt','w') as f:
+with open('jmail.txt','w') as f:
   f.write(output)
