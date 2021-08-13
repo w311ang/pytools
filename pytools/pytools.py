003deebcb1842cf7dcffdcd1776953830c8ab2bc
w311ang <30381083+w311ang@users.noreply.github.com>
2021-08-14 01:27:08 +0800

    Update pytools.py

--- pytools/pytools.py
+++ pytools/pytools.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import psutil
 
 qpass=''
 qfrom=''
@@ -73,3 +74,26 @@ def execCmd(cmd):
     text = r.read()
     r.close()
     return text
+
+def getListOfProcessSortedByMemory():
+    '''
+    Get list of running process sorted by Memory Usage
+    '''
+    listOfProcObjects = []
+    # Iterate over the list
+    for proc in psutil.process_iter():
+       try:
+           # Fetch process details as dict
+           pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+           pinfo['cpu'] = proc.cpu_percent(interval=0.5) / psutil.cpu_count()
+           # Append dict to list
+           listOfProcObjects.append(pinfo);
+       except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+           pass
+
+    # Sort list of dict by key vms i.e. memory usage
+    listOfProcObjects = sorted(listOfProcObjects, key=lambda procObj: procObj['cpu'], reverse=True)
+
+    return listOfProcObjects
+
+print(getListOfProcessSortedByMemory())
