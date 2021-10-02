e27dd3cc249e6dbcf1ec552c9854a1620cb3d9f4
w311ang <30381083+w311ang@users.noreply.github.com>
2021-10-02 11:52:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='MyPytools',
       author='w311ang',
       author_email='w311angw311ang@gmail.com',
       url='https://github.com/w311ang/pytools',
-      packages=['pytools','_aes'],
+      packages=['pytools'],
       install_requires=[
         'requests',
         'PyCryptodome',
