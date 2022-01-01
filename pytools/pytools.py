2e46364e8ace1599fab05123424aa3437832a79d
w311ang <30381083+w311ang@users.noreply.github.com>
2022-01-01 08:40:12 +0800

    Update pytools.py

--- pytools/pytools.py
+++ pytools/pytools.py
@@ -159,11 +159,15 @@ def getip(domain):
   for answer in answers:
     return answer.to_text()
 
+def bypassCC():
+  global s
+  s.headers.update({'User-Agent':'Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Mobile Safari/537.36'})
+
 passed=[]
 
 def pas(host,pw):
   s=requests.Session()
-  s.headers.update({'User-Agent':'Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Mobile Safari/537.36'})
+  bypassCC()
   s.verify=False
   requests.packages.urllib3.disable_warnings()
   with s.get('http://'+host) as web:
