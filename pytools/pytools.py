0d445c8351c23763282821c7e1a09cb0c440fb32
w311ang <30381083+w311ang@users.noreply.github.com>
2022-08-05 17:33:18 +0800

    Update pytools.py

--- pytools/pytools.py
+++ pytools/pytools.py
@@ -309,6 +309,7 @@ def set_exit_handler(func):
 def getpath(file):
     # https://stackoverflow.com/questions/53587322/how-do-i-include-files-with-pyinstaller
     import sys
+    import os
     if getattr(sys, 'frozen', False):     
         path = os.path.join(sys._MEIPASS, file)
     else:
