7fd57d09497af8548ab065e8b5af0bdc8ccad387
w311ang <30381083+w311ang@users.noreply.github.com>
2022-08-29 03:26:37 +0800

    Update pytools.py

--- pytools/pytools.py
+++ pytools/pytools.py
@@ -327,3 +327,8 @@ def checkPgmPortOpen(pgmName, port):
             if i.laddr.port==port:
                 return True
     return False
+
+def sec2time(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "%d:%02d:%02d" % (h, m, s))
