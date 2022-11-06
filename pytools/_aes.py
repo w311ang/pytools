2f5c099c0a428106f792298f56115d9bf779f20a
w311ang <30381083+w311ang@users.noreply.github.com>
2022-11-06 09:56:05 +0800

    Update _aes.py

--- pytools/_aes.py
+++ pytools/_aes.py
@@ -10,16 +10,21 @@ class AESCipher(object):
         self.key = hashlib.sha256(key.encode()).digest()
 
     def encrypt(self, raw):
+        raw = base64.b64encode(raw.encode('utf-8')).decode('utf-8')
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(raw.encode()))
 
-    def decrypt(self, enc):
+    def decrypt(self, enc, base64=True):
         enc = base64.b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        decryption = self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        if base64:
+            return base64.b64decode(decryption).decode('utf-8')
+        else:
+            return decryption
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
