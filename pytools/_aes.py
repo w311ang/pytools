9e44ea326bdbf13027e1d3ede4771f06bd362b8c
w311ang <30381083+w311ang@users.noreply.github.com>
2022-11-06 10:19:12 +0800

    Update _aes.py

--- pytools/_aes.py
+++ pytools/_aes.py
@@ -1,4 +1,4 @@
-import base64
+import base64 as b64
 import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
@@ -10,19 +10,19 @@ class AESCipher(object):
         self.key = hashlib.sha256(key.encode()).digest()
 
     def encrypt(self, raw):
-        raw = base64.b64encode(raw.encode('utf-8')).decode('utf-8')
+        raw = b64.b64encode(raw.encode('utf-8')).decode('utf-8')
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+        return b64.b64encode(iv + cipher.encrypt(raw.encode()))
 
     def decrypt(self, enc, base64=True):
-        enc = base64.b64decode(enc)
+        enc = b64.b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         dec = self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
         if base64:
-            return base64.b64decode(dec).decode('utf-8')
+            return b64.b64decode(dec).decode('utf-8')
         else:
             return decryption
 
